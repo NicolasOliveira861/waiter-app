474a27356b7ac1d3f09ad5be61081d4b7aec3c80
Nicolas Oliveira <nicksouza861@gmail.com>
2022-11-21 20:18:04 -0300

    feat: creates models

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001');
-});
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}/`);
+    });
+  })
+  .catch(() => console.log('Cannot connect to mongo!'));
